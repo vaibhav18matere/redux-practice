8d6a61f27b80554eac9346e230faee6326176276
VaibhavMatere <vpm00718897@gmail.com>
2024-09-03 22:52:10 +0530

    setup folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CakeContainer from './components/cakeContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Redux</h1>
+      <CakeContainer />
     </div>
   );
 }
---
+++ src/components/cakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+     return (
+          <>
+               <div>No of cakes</div>
+               <button>Buy cake</button>
+          </>
+     )
+}
+
+export default CakeContainer

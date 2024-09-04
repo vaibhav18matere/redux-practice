2c8c4dcccbb990c8c0e5c46ad39ae96c6e9ad0bc
VaibhavMatere <vpm00718897@gmail.com>
2024-09-04 15:56:45 +0530

    2

---
+++ src/redux/chocolate/chocolateReducer.js
@@ -0,0 +1,17 @@
+import { BUY_CHOCOLATE } from "./chocolateAction";
+
+const initialState = {
+     numOfChocolates: 30
+};
+
+const chocolateReducer = (state = initialState, action) => {
+     switch (action.type) {
+          case BUY_CHOCOLATE: return {
+               ...state,
+               numOfChocolates: state.numOfChocolates - 1
+          }
+          default: return state
+     }
+}
+
+export default chocolateReducer;
